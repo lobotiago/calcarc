e53ceac7a8d8dbcd79818eebc0b2570438677ff0
loboprogramador <tagolimas2@gmail.com>
2020-05-31 19:17:00 -0300

    Tela de inclinação do telhado concluida sem ADS

--- src/Pages/InclinacaoDoTelhado/index.js
+++ src/Pages/InclinacaoDoTelhado/index.js
@@ -1,19 +1,96 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import HeaderMenu from '../../Layouts/Header'
-import { CalculadoraCard } from '../../styles/CalculadoraCard'
+import { CalculadoraCard, CalcButton } from '../../styles/CalculadoraCard'
+import { TextField, InputAdornment, Container } from '@material-ui/core';
 
+import percent from '../../images/percent.svg'
+import centimeter from '../../images/centimeter.svg'
 // import { Container } from './styles';
 
-function InclinacaoDoTelhado() {
-  return (
-    <>
-      <HeaderMenu />
-      <CalculadoraCard>
-        <h1>Em Desenvolvimento!</h1>
-      </CalculadoraCard>
-    </>
-  ) 
-}
+export default class InclinacaoDoTelhado extends Component {
+  state = {
+    altura: 0,
+    base: null,
+    inclinacao: null
+  }
 
-export default InclinacaoDoTelhado;
+  handleAlturaChange = e => {
+    this.setState({ inclinacao: e.target.value })
+  }
+  handleBaseChange = e => {
+    this.setState({ base: e.target.value })
+  }
+
+  handleCalc = e => {
+    let inclinacao = parseInt(this.state.inclinacao)
+    let base = parseInt(this.state.base)
+    let altura = (inclinacao / 100) * base
+    if (!isNaN(altura)) {
+      this.setState({ altura: altura })
+    } else {
+      this.setState({ altura: 0 })
+    }
+  }
+
+
+  render() {
+    const { altura, base, inclinacao } = this.state
+
+    return (
+      <>
+        <HeaderMenu />
+        {/* TO DO: ADS */}
+        <Container>
+          <CalculadoraCard>
+            <h1>Cálculo de inclinação do telhado</h1>
+            <p>*consulte a inclinação mínima com o fabricante do telhado</p>
+            <TextField
+              id="inclinacao"
+              value={inclinacao}
+              label="Inclinação"
+              type="number"
+              InputProps={{
+                endAdornment: <InputAdornment position="end">
+                  <img src={percent} alt=""/>
+                </InputAdornment>
+              }}
+              variant="outlined"
+              onChange={this.handleAlturaChange}
+            />
+            <TextField
+              id="base"
+              value={base}
+              label="Base"
+              type="number"
+              InputProps={{
+                endAdornment: <InputAdornment position="end">
+                  <img src={centimeter} alt=""/>
+                </InputAdornment>
+              }}
+              variant="outlined"
+              onChange={this.handleBaseChange}
+            />
+            <CalcButton onClick={this.handleCalc}>
+              Calcular
+            </CalcButton>
+            <TextField
+              disabled
+              id="altura"
+              value={altura}
+              label="Altura"
+              InputProps={{
+                endAdornment: <InputAdornment position="end">
+                  <img src={centimeter} alt=""/>
+                </InputAdornment>,
+                readOnly: true
+              }}
+              variant="outlined"
+            />
+          </CalculadoraCard>
+        </Container>
+        {/* TO DO: ADS */}
+      </>
+    ) 
+  }
+}
--- src/styles/CalculadoraCard.js
+++ src/styles/CalculadoraCard.js
@@ -1,5 +1,53 @@
 import styled from 'styled-components';
 
 export const CalculadoraCard = styled.div`
-  margin-top: 100px;
+  display: flex;
+  margin: 100px auto;
+  background-color: #fff;
+  flex-direction: column;
+  width: max-content;
+  max-width: 100%;
+  padding: 20px;
+  border-radius: 5px;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  min-height: 60vh;
+
+  p {
+    color: #FF0000;
+    margin: 15px 0 10px
+  }
+
+  .MuiInputAdornment-positionEnd img {
+    width: 26px;
+  }
+
+  .MuiTextField-root {
+    margin-bottom: 10px;
+  }
+
+  @media screen and (max-width: 600px) {
+    h1 {
+      font-size: 20px;
+    }
+
+    p {
+      font-size: 14px;
+    }
+  }
+`
+
+export const CalcButton = styled.button`
+  margin-bottom: 16px;
+  background-color: #700098;
+  padding: 16px;
+  color: #fff;
+  font-family: 'Roboto';
+  font-size: 20px;
+  border-radius: 5px;
+  border: none;
+  cursor: pointer;
+  transition: 0.3s opacity;
+  &:hover {
+    opacity: 0.8;
+  }
 `
